ea4bcbd6fb2c10c4b085485e56a917c00d912c6c
rabra <rabira.hierpa@gmail.com>
2025-04-10 23:44:39 +0300

    feat(dashboard): Revamp Dashboard layout to utilize Ant Design components, integrating ControlPanel and MapContainer for improved UI structure

--- frontend/src/features/dashboard/page.tsx
+++ frontend/src/features/dashboard/page.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import { Layout } from "antd";
+import { Content } from "antd/es/layout/layout";
+import Sider from "antd/es/layout/Sider";
+import ControlPanel from "./components/ControlPanel";
+import MapContainer from "./components/MapContainer";
+import "@arcgis/core/assets/esri/themes/light/main.css";
 
 const DashboardPage = () => {
   return (
-    <div className="flex justify-center mt-4">
-      <div className="text-xl font-bold text-blue-500">
-        Welcome to application
-      </div>
-    </div>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Layout>
+        <Sider width={300} theme="light">
+          <ControlPanel />
+        </Sider>
+        <Content style={{ position: "relative" }}>
+          <MapContainer />
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
