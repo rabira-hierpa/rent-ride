0c682e97f6fd94f49e0b6e3143e1177966efa8ed
rabra <rabira.hierpa@gmail.com>
2025-04-10 13:31:04 +0300

    Update header styles and branding: change background color to black and update title from 'FullStack Auth' to 'Rent Ride'

--- frontend/src/shared/ui/header.tsx
+++ frontend/src/shared/ui/header.tsx
@@ -3,11 +3,11 @@ import NavMenu from "./menu/nav.menu";
 
 const Header = () => {
   return (
-    <nav className="bg-white z-50 relative  shadow-sm">
-      <div className=" container  mx-auto flex items-center h-14 space-x-6 	">
+    <nav className="bg-black z-50 relative  shadow-sm">
+      <div className=" container  mx-auto flex items-center h-14 space-x-6  	">
         <div className="text-primary-500 flex h-full items-center ">
           <Link to="/" className="text-lg font-semibold">
-            <div className="text-lg text-red-500">FullStack Auth</div>
+            <div className="text-lg text-yellow-500">Rent Ride</div>
           </Link>
         </div>
         <div className=" flex flex-grow h-full items-center justify-end space-x-6">
