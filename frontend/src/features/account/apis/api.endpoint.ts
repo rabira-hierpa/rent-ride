a6759b397654a2769294443394f5fc0f839833ea
rabra <rabira.hierpa@gmail.com>
2025-04-13 00:09:51 +0300

    refactor: Remove unused auth and admin features to simplify application structure

--- frontend/src/features/account/apis/api.endpoint.ts
+++
@@ -1,7 +0,0 @@
-import { environment } from "../../../environments/environment";
-
-export const accountEndpoint = {
-  login: `${environment.urls.api}/auth/login`,
-  register: `${environment.urls.api}/auth/register`,
-  isEmailExist: `${environment.urls.api}/auth/isEmailExist`,
-};
