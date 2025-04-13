baa92d61eb519108f4b137ad212ac47ccf4e5719
rabra <rabira.hierpa@gmail.com>
2025-04-13 16:04:06 +0300

    refactor(main.tsx): Implement lazy loading for Dashboard component and add loading spinner for improved user experience

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,28 @@
-import React, { StrictMode } from "react";
+import { StrictMode, Suspense, lazy } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css"; // Global styles
-import "antd/dist/reset.css"; // Ant Design reset
-import DashboardPage from "./features/dashboard/page";
-import { store } from "./redux/store"; // Import the store
-import { Provider } from "react-redux"; // Import the Provider
+import "./index.css";
+import "antd/dist/reset.css";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import { LoadingSpinner } from "./shared/ui/spinner/loading.spinner";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
 const root = createRoot(rootElement);
 
+const Dashboard = lazy(() =>
+  import("./features/dashboard/page").then((module) => ({
+    default: module.default,
+  }))
+);
+
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <DashboardPage />
+      <Suspense fallback={<LoadingSpinner fullScreen={true} />}>
+        <Dashboard />
+      </Suspense>
     </Provider>
   </StrictMode>
 );
